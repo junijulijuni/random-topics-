1e64908f11be9b14ace6fa5127e0f9d3587e03da
whackwhack <kathol-voilleque@campus.tu-berlin.de>
2026-04-22 11:25:00 +0200

    Create generate_toc.py

---
+++ generate_toc.py
@@ -0,0 +1,22 @@
+import os
+
+def generate_toc(path, indent=0):
+    toc = []
+    for item in sorted(os.listdir(path)):
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            if item.startswith('.'):
+                continue
+            toc.append("  " * indent + f"- [{item}]({item})")
+            toc.extend(generate_toc(item_path, indent + 1))
+        elif item.endswith('.md') and item != 'README.md':
+            toc.append("  " * indent + f"- [{item[:-3]}]({item})")
+    return toc
+
+def main():
+    toc = ["# Dance Knowledge Base", "", "## Table of Contents", ""] + generate_toc('.')
+    with open('README.md', 'w') as f:
+        f.write('\n'.join(toc))
+
+if __name__ == "__main__":
+    main()
